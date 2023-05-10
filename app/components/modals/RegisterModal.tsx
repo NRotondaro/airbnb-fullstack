a6546c8a28a738fb1f61c0558b432a18950ea73f
NRotondaro <niksonrotondaro1@gmail.com>
2023-05-10 13:51:06 -0300

    feat: finish register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,8 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import { Modal } from './Modal';
 import { Heading } from '../Heading';
 import { Input } from '../inputs/Input';
+import { toast } from 'react-hot-toast';
+import { Button } from '../Button';
 
 export const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -36,7 +38,7 @@ export const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error('Something went wrong.');
       })
       .finally(() => setIsLoading(false));
   };
@@ -71,6 +73,24 @@ export const RegisterModal = () => {
       />
     </div>
   );
+
+  const footerContent = (
+    <div className='mt-3 flex flex-col gap-4'>
+      <hr />
+      <Button outline label='Continue with Google' icon={FcGoogle} onClick={() => {}} />
+      <Button outline label='Continue with GitHub' icon={AiFillGithub} onClick={() => {}} />
+      <div className='mt-4 text-center font-light text-neutral-500'>
+        <div className='flex flex-row items-center justify-center gap-2'>
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className='cursor-pointer text-neutral-800 hover:underline'>
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
   return (
     <div>
       <Modal
@@ -81,6 +101,7 @@ export const RegisterModal = () => {
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
         body={bodyContent}
+        footer={footerContent}
       />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { RegisterModal } from './components/modals/RegisterModal';
 import { Navbar } from './components/navbar/Navbar';
 import './globals.css';
 import { Nunito } from 'next/font/google';
+import { ToasterProvider } from './providers/ToasterProvider';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={font.className}>
+        <ToasterProvider />
         <RegisterModal />
         <Navbar />
         {children}
