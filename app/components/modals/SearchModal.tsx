da0306f7b0e28c3865e667427771de3a3c9f33b2
NRotondaro <niksonrotondaro1@gmail.com>
2023-05-19 12:28:40 -0300

    feat: add filters

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -13,6 +13,7 @@ import queryString from 'query-string';
 import { formatISO } from 'date-fns';
 import { Heading } from '../Heading';
 import { Calendar } from '../inputs/Calendar';
+import { Counter } from '../inputs/Counter';
 
 enum STEPS {
   LOCATION = 0,
@@ -137,6 +138,32 @@ const SearchModal = () => {
       <div className='flex flex-col gap-8'>
         <Heading title='When do you plan to go?' subtitle='Make sure everyone is free!' />
         <Calendar value={dateRange} onChange={(value) => setDateRange(value.selection)} />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading title='More information' subtitle='Find your perfect place!' />
+        <Counter
+          title='Guests'
+          subtitle='How many guests are coming?'
+          value={guestCount}
+          onChange={(value) => setGuestCount(value)}
+        />
+        <Counter
+          title='Rooms'
+          subtitle='How many rooms do you need?'
+          value={roomCount}
+          onChange={(value) => setRoomCount(value)}
+        />
+        <Counter
+          title='Bathrooms'
+          subtitle='How many bathrooms do you need?'
+          value={bathroomCount}
+          onChange={(value) => setBathroomCount(value)}
+        />
       </div>
     );
   }
